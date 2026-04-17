326630ee88ec772b9dd035574949f691f9299649
Matt21RR <portal2matr@gmail.com>
2026-04-17 01:50:04 -0500

    Update RegisterRequest.cs

--- src/AuthService/MiniIdentityApi.Application/DTOs/RegisterRequest.cs
+++ src/AuthService/MiniIdentityApi.Application/DTOs/RegisterRequest.cs
@@ -5,4 +5,6 @@ public class RegisterRequest
     public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    public int Career { get; set; }
+    public int Semester { get; set; }
 }
