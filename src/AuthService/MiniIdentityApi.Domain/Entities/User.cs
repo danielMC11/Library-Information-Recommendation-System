6910af72570c194f316130658d55236f21c64b2e
Matt21RR <portal2matr@gmail.com>
2026-04-17 01:46:20 -0500

    Update User.cs
    Ahora acepta carrera y semestre

--- src/AuthService/MiniIdentityApi.Domain/Entities/User.cs
+++ src/AuthService/MiniIdentityApi.Domain/Entities/User.cs
@@ -9,20 +9,30 @@ public class User
     public Guid Id { get; private set; } = Guid.NewGuid();
     public string Username { get; private set; }
     public string Email { get; private set; }
+    public int Career { get; private set; }
+    public int Semester { get; private set; }
     public UserStatus Status { get; private set; } = UserStatus.Active;
     public Credential Credential { get; private set; }
     public List<Role> Roles { get; private set; } = new();
 
-    public User(string username, string email, Credential credential)
+    public User(string username, string email, int career, int semester, Credential credential)
     {
         Username = username;
         Email = email;
+        Career = career;
+        Semester = semester;
         Credential = credential;
     }
 
     public void Activate() => Status = UserStatus.Active;
     public void Deactivate() => Status = UserStatus.Inactive;
     public void Block() => Status = UserStatus.Blocked;
+
+    public void UpdateCareerAndSemester(int career, int semester)
+    {
+        Career = career;
+        Semester = semester;
+    }
 
     public void AddRole(Role role)
     {
